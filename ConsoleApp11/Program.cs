ff401a1faeb8088d99b263e32477b5be877fc294
jonathanwirz <jonathan.wirz@icloud.com>
2024-10-02 07:25:40 +0200

    Aufgaben

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie lange soll die Linie sein?");
+            if (int.TryParse(Console.ReadLine(), out int length))
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    for (int j = 0; j < length; j++)
+                    {
+                        if (i == j)
+                        {
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            Console.Write("*");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bitte gib eine gültige Zahl ein.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Prüfen, ob es sich um ein Schaltjahr handelt.");
+        Console.WriteLine("Bitte gib ein Jahr ein:");
+        if (int.TryParse(Console.ReadLine(), out int year))
+        {
+            string resultat = CheckSchaltjahr(year);
+            Console.WriteLine(resultat);
+        }
+        else
+        {
+            Console.WriteLine("Bitte gib eine gültige Zahl ein.");
+        }
+    }
+
+    static string CheckSchaltjahr(int year)
+    {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        {
+            return "Das Jahr " + year + " ist ein Schaltjahr.";
+        }
+        else
+        {
+            return "Das Jahr " + year + " ist kein Schaltjahr.";
+        }
+    }
+}
